4a4b4fe0193874ef15d480e43259b9970115b047
dev-chulbuji <dev.chulbuji@gmail.com>
2021-02-26 13:08:57 +0900

    set multiple upstream servers

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res, next) => {
-  res.json('thomas genius')
+  const sourceIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  res.json(`source ip :: ${sourceIp}`)
 })
 
 app.listen(port, () => console.log(`server is running on ${port}`))
